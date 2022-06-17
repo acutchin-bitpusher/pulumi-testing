8049d5a254e5fab89f6ffe77619daf8aba3a55b0
Aaron Cutchin <acutchin@bitpusher.com>
2022-06-17 16:03:20 -0700

    working vpc_net project

--- vpc_net/__main__.py
+++ vpc_net/__main__.py
@@ -4,14 +4,8 @@ import pulumi_gcp as gcp
 config = pulumi.Config()
 pulumi_stack_name = pulumi.get_stack()
 pulumi_project_name = pulumi.get_project()
-print( "   pulumi_stack_name: ", pulumi_stack_name )
-print( " pulumi_project_name: ", pulumi_project_name )
-
 resource_name_prefix = pulumi_project_name + "-" + pulumi_stack_name
-print( "resource_name_prefix: ", resource_name_prefix )
-
 env_config = config.require_object("env_config")
-
 commontags = {
   "project": pulumi_project_name,
   "stack": pulumi_stack_name,
@@ -30,13 +24,13 @@ network = gcp.compute.Network(
   ## resource_name; positional first arg; must match regex: '(?:[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?)',
   resource_name_prefix,
   name = resource_name_prefix,
-  description = env_config["vpc_net"]["description"],
-  routing_mode = env_config["vpc_net"]["routing_mode"],
+  description = env_config["description"],
+  routing_mode = env_config["routing_mode"],
 )
-pulumi.export( "vpc_net_gateway_ipv4", network.gateway_ipv4 )
-pulumi.export( "vpc_net_id", network.id )
+pulumi.export( "vpc-net_gateway_ipv4", network.gateway_ipv4 )
+pulumi.export( "vpc-net_id", network.id )
 ##  "selfLInk" is supposed to be an output according to: https://www.pulumi.com/registry/packages/gcp/api-docs/compute/network/#id_python
-pulumi.export( "vpc_net_self_link", network.self_link )
-pulumi.export( "vpc_network", network )
+pulumi.export( "vpc-net_self_link", network.self_link )
+pulumi.export( "vpc-network", network )
 
 
