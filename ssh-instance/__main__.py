d9b60cf82fddd70b14a7b45c6b765768131102ad
Aaron Cutchin <acutchin@bitpusher.com>
2022-06-20 12:45:02 -0700

    working w/ firewall

--- ssh-instance/__main__.py
+++ ssh-instance/__main__.py
@@ -18,30 +18,27 @@ commontags = {
   "stack": pulumi_stack_name,
 }
 
-vpc_net_stack_ref = pulumi.StackReference( f"{pulumi_org_name}/acbptest-network/{pulumi_stack_name}" )
-pulumi.export( "vpc_network_self_link", vpc_net_stack_ref.get_output("vpc_net_self_link") )
-
-##  CAN'T PRINT AN OUTPUT (EVEN FROM ANOTHER STACK)
-##  BECAUSE AN OUTPUT IS A CONTAINER FOR A DEFERRED VALUE THAT IS NOT YET AVAILABLE WHEN PRINT IS CALLED
-##  PRINTS SOMETHING LIKE: "<pulumi.output.Output object at 0x106c3e2e0>"
-#print( "vpc_network_self_link: ", vpc_net_stack_ref.get_output("vpc_net_self_link") )
-
-vpc_network = vpc_net_stack_ref.get_output( "vpc_network" )
-pulumi.export( "vpc_network_name", vpc_network["name"] )
+vpc_net_stack_ref = pulumi.StackReference( pulumi_org_name + "/" + env_config["vpc_net_pulumi_project_name"] + "/" + pulumi_stack_name )
+pulumi.export( "vpc_net_self_link", vpc_net_stack_ref.get_output("vpc_net_self_link") )
+vpc_net = vpc_net_stack_ref.get_output( "vpc-net" )
+pulumi.export( "vpc_net_name", vpc_net["name"] )
 
 firewall = gcp.compute.Firewall(
   resource_name_prefix,
   #project = pulumi_project_name,
-  network = vpc_net_stack_ref.get_output("vpc_net_self_link"),
+  #network = vpc_net_stack_ref.get_output("vpc_net_self_link"),
+  network = vpc_net["name"],
+  direction = "INGRESS",
+  source_ranges = [ "0.0.0.0/0" ],
   #source_tags = ["ssh-ingress"],
   target_tags = ["ssh-ingress"],
   allows = [
     gcp.compute.FirewallAllowArgs(
-      protocol="icmp"
+      protocol = "icmp"
     ),
     gcp.compute.FirewallAllowArgs(
-      protocol="tcp",
-      ports=["22", "80", "443"],
+      protocol = "tcp",
+      ports = ["22", "80", "443"],
     ),
   ],
 )
@@ -53,40 +50,40 @@ firewall = gcp.compute.Firewall(
 #  display_name="Service Account"
 #  #display_name = ( resource_name_prefix + "-" + "ssh-instance" ),
 #)
-ssh_instance = gcp.compute.Instance(
-  resource_name_prefix,
-  name = resource_name_prefix,
-  machine_type = "f1-micro",
-  zone = ( env_config["gcp_region"] + "-a" ),
-  tags = [
-    "ssh-ingress",
-  ],
-  boot_disk=gcp.compute.InstanceBootDiskArgs(
-    initialize_params=gcp.compute.InstanceBootDiskInitializeParamsArgs(
-      image="debian-cloud/debian-9",
-    ),
-  ),
+#ssh_instance = gcp.compute.Instance(
+#  resource_name_prefix,
+#  name = resource_name_prefix,
+#  machine_type = "f1-micro",
+#  zone = ( env_config["gcp_region"] + "-a" ),
+#  tags = [
+#    "ssh-ingress",
+#  ],
+#  boot_disk=gcp.compute.InstanceBootDiskArgs(
+#    initialize_params=gcp.compute.InstanceBootDiskInitializeParamsArgs(
+#      image="debian-cloud/debian-9",
+#    ),
+#  ),
 #  scratch_disks=[gcp.compute.InstanceScratchDiskArgs(
 #    interface="SCSI",
 #  )],
-  network_interfaces=[
-    gcp.compute.InstanceNetworkInterfaceArgs(
-      #network="default",
-      network = vpc_net_stack_ref.get_output("vpc_net_self_link"),
-      access_configs = [
-        gcp.compute.InstanceNetworkInterfaceAccessConfigArgs()
-      ],
-    )
-  ],
-  metadata={
-    "foo": "bar",
-  },
-  metadata_startup_script="echo hi > /test.txt",
+#  network_interfaces=[
+#    gcp.compute.InstanceNetworkInterfaceArgs(
+#      #network="default",
+#      network = vpc_net_stack_ref.get_output("vpc_net_self_link"),
+#      access_configs = [
+#        gcp.compute.InstanceNetworkInterfaceAccessConfigArgs()
+#      ],
+#    )
+#  ],
+#  metadata={
+#    "foo": "bar",
+#  },
+#  metadata_startup_script="echo hi > /test.txt",
 #  service_account = gcp.compute.InstanceServiceAccountArgs(
 #    email = default_account.email,
 #    scopes = ["cloud-platform"],
 #  )
-)
-pulumi.export('instance_name', ssh_instance.name)
-pulumi.export('instance_meta_data', ssh_instance.metadata)
-pulumi.export('instance_network', ssh_instance.network_interfaces)
+#)
+#pulumi.export('instance_name', ssh_instance.name)
+#pulumi.export('instance_meta_data', ssh_instance.metadata)
+#pulumi.export('instance_network', ssh_instance.network_interfaces)
