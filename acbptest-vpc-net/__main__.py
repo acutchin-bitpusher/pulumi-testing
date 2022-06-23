d7faf260a3d76ecb60652390c405e9a03019f8b3
Aaron Cutchin <acutchin@bitpusher.com>
2022-06-23 14:02:32 -0700

    working and deploying vpc network in ac@bp GCP account under UV Pulumi org/project

--- acbptest-vpc-net/__main__.py
+++ acbptest-vpc-net/__main__.py
@@ -4,16 +4,12 @@ import pulumi_gcp as gcp
 config = pulumi.Config()
 pulumi_stack_name = pulumi.get_stack()
 pulumi_project_name = pulumi.get_project()
-resource_name_prefix = pulumi_project_name + "-" + pulumi_stack_name
+pulumi_proj_stack = pulumi_project_name + "-" + pulumi_stack_name
 env_config = config.require_object("env_config")
-commontags = {
-  "project": pulumi_project_name,
-  "stack": pulumi_stack_name,
-}
 
 ##  EXTERNAL IP ADDRESS
 ip_addr = gcp.compute.Address(
-  resource_name = resource_name_prefix
+  resource_name = pulumi_proj_stack
 )
 external_ip = ip_addr.address
 pulumi.export('external_ip', ip_addr.address)
@@ -21,9 +17,9 @@ pulumi.export('external_ip', ip_addr.address)
 ##  VPC/NETWORK
 ##  https://www.pulumi.com/registry/packages/gcp/api-docs/compute/network/
 network = gcp.compute.Network(
-  ## resource_name; positional first arg; must match regex: '(?:[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?)',
-  resource_name_prefix,
-  name = resource_name_prefix,
+  ## resource_name; positional first arg; must match regex: '(?:[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?)'
+  pulumi_proj_stack,
+  name = pulumi_proj_stack,
   description = env_config["description"],
   routing_mode = env_config["routing_mode"],
 )
